2546afcd4d5c33b384de0607986d4dd91782019b
mitakshi-chandna <mitakshi.chandna@flipkart.com>
2024-08-08 11:53:04 +0530

    deleting src

--- src/com/flipkart/bean/Admin.java
+++
@@ -1,7 +0,0 @@
-package com.flipkart.bean;
-
-public class Admin {
-    private int adminId;
-    private String name;
-
-}
--- src/com/flipkart/bean/Course.java
+++
@@ -1,8 +0,0 @@
-package com.flipkart.bean;
-
-public class Course {
-    private String courseCode;
-    private String name;
-    private String instructor;
-    private Boolean isOffered;
-}
