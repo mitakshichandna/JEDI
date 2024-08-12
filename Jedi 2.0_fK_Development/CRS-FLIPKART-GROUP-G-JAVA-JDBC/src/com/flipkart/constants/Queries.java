d08c8bf14ac613fe9293f00c96c81e3f25852f45
Saahir999-3 <saahir.j@flipkart.com>
2024-08-13 00:53:00 +0530

    Admin DAO implemented

--- Jedi 2.0_fK_Development/CRS-FLIPKART-GROUP-G-JAVA-JDBC/src/com/flipkart/constants/Queries.java
+++ Jedi 2.0_fK_Development/CRS-FLIPKART-GROUP-G-JAVA-JDBC/src/com/flipkart/constants/Queries.java
@@ -4,6 +4,7 @@ public class Queries{
     public static final String DELETE_COURSE = "delete from Course where courseId = ?";
     public static final String ADD_COURSE = "insert into Course(courseId, courseName) values (?, ?)";
 	public static final String REGISTER_STUDENT = "update Student set isRegistered = 1 where studentId = ?";
+	public static final String STUDENT_COURSES = "select courseId from StudentCourses where studentId = ?";
 	public static final String ASSIGN_COURSE = "update Course set professorId = ? where courseId = ?";
 	public static final String VIEW_COURSE = "select courseId, courseName, professorId from Course";
 	public static final String LIST_REGISTERED_STUDENTS = "select studentId, name from Student where isRegistered = 0";
--- Jedi 2.0_fK_Development/CRS-FLIPKART-GROUP-G-JAVA-JDBC/src/com/flipkart/dao/AdminDAOInterface.java
+++ Jedi 2.0_fK_Development/CRS-FLIPKART-GROUP-G-JAVA-JDBC/src/com/flipkart/dao/AdminDAOInterface.java
@@ -1,12 +1,15 @@
 package com.flipkart.dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.flipkart.constants.Queries;
 import com.flipkart.utils.DButils;
+import com.flipkart.utils.Pair;
 
 /*
  * Admins listed under table Admin(adminId(int, suto inc, unique, non null, prime), name(VARCHAR(45)))
@@ -21,42 +24,77 @@ public class AdminDAOInterface {
         try{
             statement = connection.prepareStatement(Queries.DELETE_COURSE);
             statement.setInt(1, courseId);
-            
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            // TODO: handle exception
+        }
+    }
+    public void addCourse(int courseId, String courseName){
+        try{
+            statement = connection.prepareStatement(Queries.ADD_COURSE);
+            statement.setInt(1, courseId);
+            statement.setString(2, courseName);
+            statement.executeUpdate();
         } catch (Exception e) {
             // TODO: handle exception
         }
     }
-    public void addCourse(){
+    public List<Integer> getStudentCourses(int studentId){
+        ArrayList<Integer> courses = new ArrayList<Integer>(10);
         try{
-            
+            statement = connection.prepareStatement(Queries.STUDENT_COURSES);
+            statement.setInt(1, studentId);
+            ResultSet set = statement.executeQuery();
+            while(set.next()){
+                courses.add(set.getInt("course"));
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        return courses;
+    }
+    public void approveStudent(int studentId){
+        try{
+            statement = connection.prepareStatement(Queries.REGISTER_STUDENT);
+            statement.setInt(1, studentId);
+            statement.executeUpdate();
         } catch (Exception e) {
             // TODO: handle exception
         }
     }
-    public void approveStudentReg(){
+    public List<Pair<Integer, String>> registeredStudents(int studentId){
+        ArrayList<Pair<Integer, String>> courses = new ArrayList<Pair<Integer, String>>(10);
         try{
-            
+            statement = connection.prepareStatement(Queries.LIST_REGISTERED_STUDENTS);
+            ResultSet set = statement.executeQuery();
+            while(set.next()){
+                courses.add(new Pair<Integer, String>(set.getInt("studentId"), set.getString("name")));
+            }
         } catch (Exception e) {
             // TODO: handle exception
         }
+        return courses;
     }
-    public void registeredStudents(){
+    public List<Pair<Integer, String>> unregisteredStudents(){
+        ArrayList<Pair<Integer, String>> courses = new ArrayList<Pair<Integer, String>>(10);
         try{
-            
+            statement = connection.prepareStatement(Queries.LIST_UNREGISTERED_STUDENTS);
+            ResultSet set = statement.executeQuery();
+            while(set.next()){
+                courses.add(new Pair<Integer, String>(set.getInt("studentId"), set.getString("name")));
+            }
         } catch (Exception e) {
             // TODO: handle exception
         }
+        return courses;
     }
-    public void unregisteredStudents(){
+    public void assignProfToCourse(int professorId, int courseId){
         try{
-            
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-    }
-    public void assignProfToCourse(){
-        try{
-            
+            statement = connection.prepareStatement(Queries.ASSIGN_COURSE);
+            statement.setInt(1, professorId);
+            statement.setInt(2, courseId);
+            statement.executeUpdate();
         } catch (Exception e) {
             // TODO: handle exception
         }
