d3aa33bf52b79eabfee74449057fb68a011e2a12
Saahir999-3 <saahir.j@flipkart.com>
2024-08-12 14:40:36 +0530

    Global DB Connection implemented via DButils

---
+++ Jedi 2.0_fK_Development/CRS-FLIPKART-GROUP-G-JAVA-JDBC/src/com/flipkart/utils/DButils.java
@@ -0,0 +1,32 @@
+package com.flipkart.utils;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+import com.flipkart.utils.DButils;
+
+public class DButils {
+    private static Connection connection;
+    public static Connection getConnection(){
+        if(connection != null){
+            return connection;
+        }else{
+            try{
+                Properties prop = new Properties();
+                InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("../../../../config.properties");
+                prop.load(inputStream);
+                String driver = prop.getProperty("driver");
+                String url = prop.getProperty("url");
+                String user = prop.getProperty("user");
+                String password = prop.getProperty("password");
+                Class.forName(driver);
+                connection = DriverManager.getConnection(url, user, password);
+            }catch(Exception e){
+
+            }
+            return connection;
+        }
+    }
+}
