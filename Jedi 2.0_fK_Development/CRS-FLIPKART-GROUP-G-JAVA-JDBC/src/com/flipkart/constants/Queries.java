36ef8d9857c0b99e94953b0afb7c08dadc2a6e6f
Saahir999-3 <saahir.j@flipkart.com>
2024-08-12 15:32:41 +0530

    Created AdminDao Skeleton

--- Jedi 2.0_fK_Development/CRS-FLIPKART-GROUP-G-JAVA-JDBC/src/com/flipkart/constants/Queries.java
+++ Jedi 2.0_fK_Development/CRS-FLIPKART-GROUP-G-JAVA-JDBC/src/com/flipkart/constants/Queries.java
@@ -2,9 +2,9 @@ package com.flipkart.constants;
 
 public class Queries{
     public static final String DELETE_COURSE = "delete from Course where courseId = ?";
-    public static final String ADD_COURSE = "insert into Course(courseId, coureName) values (?, ?)";
+    public static final String ADD_COURSE = "insert into Course(courseId, courseName) values (?, ?)";
 	public static final String REGISTER_STUDENT = "update Student set isRegistered = 1 where studentId = ?";
-	public static final String ASSIGN_COURSE = "update Course set professorId = ? where courseCode = ?";
+	public static final String ASSIGN_COURSE = "update Course set professorId = ? where courseId = ?";
 	public static final String VIEW_COURSE = "select courseId, courseName, professorId from Course";
 	public static final String LIST_REGISTERED_STUDENTS = "select studentId, name from Student where isRegistered = 0";
     public static final String LIST_UNREGISTERED_STUDENTS = "select studentId, name from Student where isRegistered = 1";
